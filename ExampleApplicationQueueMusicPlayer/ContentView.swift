f3a2f012eea733e86f41c80d82ec3d297d6f05c0
Jake Nelson <j.n@live.com.au>
2021-06-26 20:16:21 +0800

    Initial Commit

---
+++ ExampleApplicationQueueMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleApplicationQueueMusicPlayer
+//
+//  Created by Jake Nelson on 26/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApplicationQueueMusicPlayer/ExampleApplicationQueueMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApplicationQueueMusicPlayerApp.swift
+//  ExampleApplicationQueueMusicPlayer
+//
+//  Created by Jake Nelson on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApplicationQueueMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
